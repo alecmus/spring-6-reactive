30f1577b3be9b646b12704131afe7c77c4ea8d4f
Alec Musasa <alecmus@live.com>
2023-06-27 19:18:08 +0200

    Added Spring Initializr code

---
+++ src/test/java/com/github/alecmus/spring6reactive/Spring6ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.alecmus.spring6reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
