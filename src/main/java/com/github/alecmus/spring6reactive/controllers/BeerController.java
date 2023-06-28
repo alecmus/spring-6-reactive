9264f45d42486e20d30f9d4cfcbe5f27d764e471
Alec Musasa <alecmus@live.com>
2023-06-28 15:57:15 +0200

    Create new Beer entity

--- src/main/java/com/github/alecmus/spring6reactive/controllers/BeerController.java
+++ src/main/java/com/github/alecmus/spring6reactive/controllers/BeerController.java
@@ -3,9 +3,9 @@ package com.github.alecmus.spring6reactive.controllers;
 import com.github.alecmus.spring6reactive.model.BeerDTO;
 import com.github.alecmus.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -17,6 +17,16 @@ public class BeerController {
     public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
 
     private final BeerService beerService;
+
+    @PostMapping(BEER_PATH)
+    Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO) {
+        return beerService.saveNewBeer(beerDTO)
+                .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
+                        .fromHttpUrl("http://localhost:8080/" + BEER_PATH
+                                + "/" + savedDto.getId())
+                                .build().toUri())
+                        .build());
+    }
 
     @GetMapping(BEER_PATH_ID)
     Mono<BeerDTO> getBeerById(@PathVariable("beerId") Integer beerId) {
--- src/main/java/com/github/alecmus/spring6reactive/services/BeerService.java
+++ src/main/java/com/github/alecmus/spring6reactive/services/BeerService.java
@@ -9,4 +9,6 @@ public interface BeerService {
     Flux<BeerDTO> listBeers();
 
     Mono<BeerDTO> getBeerById(Integer beerId);
+
+    Mono<BeerDTO> saveNewBeer(BeerDTO beerDTO);
 }
