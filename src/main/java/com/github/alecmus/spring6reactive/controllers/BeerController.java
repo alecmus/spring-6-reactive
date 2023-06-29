5bd607260b31cfd01f56d25b4dd50289882ebabb
Alec Musasa <alecmus@live.com>
2023-06-29 12:39:42 +0200

    Added validation

--- src/main/java/com/github/alecmus/spring6reactive/controllers/BeerController.java
+++ src/main/java/com/github/alecmus/spring6reactive/controllers/BeerController.java
@@ -4,6 +4,7 @@ import com.github.alecmus.spring6reactive.model.BeerDTO;
 import com.github.alecmus.spring6reactive.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
@@ -28,20 +29,20 @@ public class BeerController {
 
     @PatchMapping(BEER_PATH_ID)
     Mono<ResponseEntity<Void>> patchExistingBeer(@PathVariable Integer beerId,
-                                                 @RequestBody BeerDTO beerDTO){
+                                                 @Validated @RequestBody BeerDTO beerDTO){
         return beerService.patchBeer(beerId, beerDTO)
                 .map(updatedDto -> ResponseEntity.ok().build());
     }
 
     @PutMapping(BEER_PATH_ID)
     Mono<ResponseEntity<Void>> updateExistingBeer(@PathVariable("beerId") Integer beerId,
-                                                  @RequestBody BeerDTO beerDTO){
+                                                  @Validated @RequestBody BeerDTO beerDTO){
         return beerService.updateBeer(beerId, beerDTO)
                 .map(savedDto -> ResponseEntity.ok().build());
     }
 
     @PostMapping(BEER_PATH)
-    Mono<ResponseEntity<Void>> createNewBeer(@RequestBody BeerDTO beerDTO){
+    Mono<ResponseEntity<Void>> createNewBeer(@Validated @RequestBody BeerDTO beerDTO){
         return beerService.saveNewBeer(beerDTO)
                 .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
                                 .fromHttpUrl("http://localhost:8080/" + BEER_PATH
