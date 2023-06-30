745181306daf45d293022811993f534926182184
Alec Musasa <alecmus@live.com>
2023-06-30 14:10:41 +0200

    Test create Beer entity

--- src/test/java/com/github/alecmus/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/com/github/alecmus/spring6reactive/controllers/BeerControllerTest.java
@@ -1,10 +1,13 @@
 package com.github.alecmus.spring6reactive.controllers;
 
+import com.github.alecmus.spring6reactive.model.BeerDTO;
+import com.github.alecmus.spring6reactive.repositories.BeerRepositoryTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootTest
 @AutoConfigureWebTestClient
@@ -12,6 +15,25 @@ class BeerControllerTest {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    void testCreateBeer() {
+        webTestClient.post().uri(BeerController.BEER_PATH)
+                .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().location("http://localhost:8080/api/v2/beer/4");
+    }
+
+    @Test
+    void testGetById() {
+        webTestClient.get().uri(BeerController.BEER_PATH_ID, 1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(BeerDTO.class);
+    }
 
     @Test
     void testListBeers() {
