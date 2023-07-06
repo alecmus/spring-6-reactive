c31eeb77898749e4c5d243dab447eea06eb3fef8
Alec Musasa <alecmus@live.com>
2023-07-06 15:02:57 +0200

    Added Spring Security Configuration
    This sets this up as a resource server that uses JWT tokens for authentication. Note the similarities and differences with the MVC equivalent. For instance, where MVC uses a SecurityFilterChain here we use a SecurityWebFilterChain

---
+++ src/main/java/com/github/alecmus/spring6reactive/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.github.alecmus.spring6reactive.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer().jwt();
+
+        return http.build();
+    }
+}
